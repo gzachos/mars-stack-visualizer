ecb17675467b89ad2d4fa4cd3aa6d922cc81e25b
George Z. Zachos <gzachos@cse.uoi.gr>
2018-02-20 22:25:21 +0200

    Added mars/tools/StackVisualizer.java

---
+++ mars/tools/StackVisualizer.java
@@ -0,0 +1,78 @@
+package mars.tools;
+
+import java.awt.Dimension;
+import java.awt.GridBagLayout;
+import java.util.Observable;
+
+import javax.swing.JComponent;
+import javax.swing.JLabel;
+import javax.swing.JPanel;
+
+import mars.mips.hardware.AccessNotice;
+import mars.mips.hardware.Memory;
+import mars.mips.hardware.MemoryAccessNotice;
+import mars.mips.hardware.Register;
+import mars.mips.hardware.RegisterAccessNotice;
+import mars.mips.hardware.RegisterFile;
+
+@SuppressWarnings("serial")
+public class StackVisualizer extends AbstractMarsToolAndApplication {
+
+	private static String name = "Stack Visualizer";
+	private static String version = "Version 0.1 (George Z. Zachos, Petros Manousis)";
+	private static String heading = "Visualizing stack modification operations";
+
+	protected StackVisualizer(String title, String heading) {
+		super(title, heading);
+	}
+
+	public StackVisualizer() {
+		super(StackVisualizer.name + ", " + StackVisualizer.version, StackVisualizer.heading);
+	}
+
+	@Override
+	public String getName() {
+		return name;
+	}
+
+	@Override
+	protected JComponent buildMainDisplayArea() {
+		JPanel panel = new JPanel(new GridBagLayout());
+		panel.setPreferredSize(new Dimension(600, 650));
+		panel.add(new JLabel("Test Label Content"));
+		return panel;
+	}
+	
+	// @Override
+	protected void addAsObserver() {
+		addAsObserver(Memory.stackLimitAddress, Memory.stackBaseAddress);
+		addAsObserver(RegisterFile.getRegisters()[RegisterFile.STACK_POINTER_REGISTER]);
+	}
+
+	
+	@Override
+	protected void processMIPSUpdate(Observable resource, AccessNotice notice) {
+
+		if (notice instanceof MemoryAccessNotice)
+		{
+			MemoryAccessNotice m = (MemoryAccessNotice) notice;
+			System.out.println("MemoryAccessNotice (" + 
+					((m.getAccessType() == AccessNotice.READ) ? "R" : "W") + "): "
+					+ hex(m.getAddress()) + " value: " + m.getValue());
+		}
+		if (notice instanceof RegisterAccessNotice)
+		{
+			RegisterAccessNotice r = (RegisterAccessNotice) notice;
+			if (r.getAccessType() == AccessNotice.READ)
+				return;
+			System.out.println("RegisterAccessNotice (W): " + r.getRegisterName());
+		}
+		
+		super.processMIPSUpdate(resource, notice);
+	}
+	
+	private static String hex(int dec) {
+		return Integer.toHexString(dec);
+	}
+	
+}
